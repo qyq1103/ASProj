4ed6362c625813a3c0703e0865876d6bb7a7a880
qyq1103 <42115875+qyq1103@users.noreply.github.com>
2023-05-26 11:17:35 +0800

    新增common模块、初始化Log管理器 2023/05/26

---
+++ app/src/main/java/org/qyq/as/proj/main/App.kt
@@ -0,0 +1,16 @@
+package org.qyq.`as`.proj.main
+
+import org.qyq.`as`.proj.common.component.HiBaseApplication
+
+/**
+ *
+ * @Author: Net Spirit
+ * @Time: 2023/5/24 23:01
+ * @FixAuthor:
+ * @FixTime:
+ * @Desc:
+ *
+ */
+class App :HiBaseApplication() {
+
+}
